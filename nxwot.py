7a8a37cc9d8d10dd3e4476c08d5a5a1217cb9aa9
Corey Farwell <coreyf@rwell.org>
2013-11-17 19:53:04 -0800

    small changes

--- nxwot.py
+++ nxwot.py
@@ -8,6 +8,17 @@ def parse_nodes(g, keysfile, namesfile):
 def parse_edges(g, sigfile):
     raise NotImplementedError
 
+def get_files(wot_filename):
+    with bz2.BZ2File(wot_filename) as wot_ar:
+        wot_archive = arpy.Archive(fileobj=wot_ar)
+        wot_files = {}
+
+        for f in wot_archive:
+            filename = f.header.name.decode()
+            wot_files[filename] = f.read()
+
+        return wot_files
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.stderr.write("nxwot.py <WOT file>\n")
@@ -15,5 +26,4 @@ if __name__ == "__main__":
 
 
     filename = sys.argv[1]
-
-    print(bz2.BZ2File(filename))
+    files = get_files(filename)
