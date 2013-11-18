686a1839af9a7cc1b02ff2de5c46599dbc945b78
Corey Farwell <coreyf@rwell.org>
2013-11-17 19:49:46 -0800

    small changes

--- nxwot.py
+++ nxwot.py
@@ -1,12 +1,25 @@
 import bz2
 import sys
 
-
-if len(sys.argv) < 2:
-    sys.stderr.write("nxwot.py <WOT file>\n")
-    sys.exit(1)
+import arpy
 
 
-filename = sys.argv[1]
+def get_files(wot_filename):
+    with bz2.BZ2File(wot_filename) as wot_ar:
+        wot_archive = arpy.Archive(fileobj=wot_ar)
+        wot_files = {}
 
-print(bz2.BZ2File(filename))
+        for f in wot_archive:
+            filename = f.header.name.decode()
+            wot_files[filename] = f.read()
+
+        return wot_files
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.stderr.write("nxwot.py <WOT file>\n")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    files = get_files(filename)
