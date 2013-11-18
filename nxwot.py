bd92c19014899b557bcc49f58ebf41dfc337e138
Corey Farwell <coreyf@rwell.org>
2013-11-17 19:56:19 -0800

    No need for lines to be inside with block

--- nxwot.py
+++ nxwot.py
@@ -14,15 +14,15 @@ def parse_edges(g, sigfile):
 
 
 def get_files(wot_filename):
+    wot_files = {}
+
     with bz2.BZ2File(wot_filename) as wot_ar:
         wot_archive = arpy.Archive(fileobj=wot_ar)
-        wot_files = {}
-
         for f in wot_archive:
             filename = f.header.name.decode()
             wot_files[filename] = f.read()
 
-        return wot_files
+    return wot_files
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
